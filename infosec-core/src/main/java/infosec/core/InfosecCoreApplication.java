951e28c0fc7644eae8e207f354ce3974c69d2f46
筠之 <yunzhi.yq@antgroup.com>
2023-01-18 15:00:03 +0800

    init infosec-core

---
+++ infosec-core/src/main/java/infosec/core/InfosecCoreApplication.java
@@ -0,0 +1,13 @@
+package infosec.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfosecCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfosecCoreApplication.class, args);
+    }
+
+}
---
+++ infosec-core/src/test/java/infosec/core/InfosecCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package infosec.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfosecCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
